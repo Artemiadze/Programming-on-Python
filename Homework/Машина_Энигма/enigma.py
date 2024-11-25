8986c71e80eed7ecb924396a933020ba0c3ec3b9
Artemiadze <t0mi.v@yandex.ru>
2024-11-25 22:53:12 +0300

    Add rotor's metods in class Enigma

--- Homework/Машина_Энигма/enigma.py
+++ Homework/Машина_Энигма/enigma.py
@@ -1,17 +1,27 @@
 class Enigma:
-    def __init__(self, message, panel):
+    def __init__(self, message, panel, rotor_1, rotor_2, rotor_3):
         self.message = message  # сообщение, которое нужно закодировать
         self.alphabet = "АБВГ"  # язык, который используется при кодировке
         self.panel = panel      # list элементов соединительной панели
+        self.rotor_1 = rotor_1  # начальная комбинация первого ротора
+        self.rotor_2 = rotor_2  # начальная комбинация второго ротора
+        self.rotor_3 = rotor_3  # начальная комбинация третьего ротора
 
     def panel_changes(self):
-        for i in range(0, len(self.message)):
+        """ Метод производит замену символов согласно правилу, прописанном в соединительной панели"""
+        for element in self.message:
             for j in range(0, len(self.panel), 2):
-                if self.message[i] == self.panel[j]:
-                    self.message = self.message.replace(self.message[i], self.panel[j+1])
-                elif self.message[i] == self.panel[j+1]:
-                    self.message = self.message.replace(self.message[i], self.panel[j])
+                if element == self.panel[j]:
+                    self.message = self.message.replace(element, self.panel[j+1])
+                elif element == self.panel[j+1]:
+                    self.message = self.message.replace(element, self.panel[j])
         print(self.message)
+
+    def first_rotor_change(self):
+        """Метод совершает изменение сообщения по первому ротору"""
+
+    def update_rotor(self):
+        """Метод совершает сдвиг букв в роторе после нажатия клавиши"""
         
 
 
--- Homework/Машина_Энигма/my_file.py
+++ Homework/Машина_Энигма/my_file.py
@@ -4,7 +4,20 @@ with open("Connecting_Panel.txt", "r", encoding='utf-8') as file:
     # четные индексы - буквы которые заменяются, нечетные - которые надо заменить
     file_with_panel = re.split(r'[ ,-]', file.read())
 
-print(file_with_panel)
+with open("Rotor_1.txt", "r", encoding='utf-8') as file_1:
+    file_rotor_1 = file_1.read()
 
-code = e.Enigma('ГАВ',file_with_panel)
+with open("Rotor_2.txt", "r", encoding='utf-8') as file_1:
+    file_rotor_2 = file_1.read()
+
+with open("Rotor_3.txt", "r", encoding='utf-8') as file_1:
+    file_rotor_3 = file_1.read()
+
+# Не забыть удалить!!!
+print(file_with_panel)
+print(file_rotor_1)
+print(file_rotor_2)
+print(file_rotor_3)
+
+code = e.Enigma('ГАВ', file_with_panel, file_rotor_1, file_rotor_2, file_rotor_3)
 code.panel_changes()
