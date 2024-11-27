b5e039a6d2c19b3ea4aaf2268b5bdaae63365ca4
Artemiadze <t0mi.v@yandex.ru>
2024-11-27 13:38:00 +0300

    add class Rotors in Enigma

--- Homework/Машина_Энигма/enigma.py
+++ Homework/Машина_Энигма/enigma.py
@@ -1,7 +1,7 @@
-class Enigma():
-    def __init__(self, alphabet, panel, rotor_1, rotor_2, rotor_3, reflector):
+class Rotors:
+    """Класс ротор, включающий в себя методы имитации работы роторов в Энигме"""
+    def __init__(self, alphabet, rotor_1, rotor_2, rotor_3):
         self.alphabet = alphabet  # наш алфавит
-        self.panel = panel  # list элементов соединительной панели
 
         self.rotor_1 = rotor_1  # начальная комбинация первого ротора для левого смещения
         self.rotor_2 = rotor_2  # начальная комбинация второго ротора для левого смещения
@@ -11,13 +11,19 @@ class Enigma():
         self.rotor_2_start = rotor_2  # начальная комбинация второго ротора для правого смещения
         self.rotor_3_start = rotor_3  # начальная комбинация третьего ротора для правого смещения
 
-        self.reflector = reflector  # комбинация рефлектора
-
         self.rotor_shift_left_1 = 0  # сколько сдвигов влево совершил первый ротер
         self.rotor_shift_left_2 = 0  # сколько сдвигов влево совершил второй ротер
 
         self.rotor_shift_right_1 = 0  # сколько сдвигов вправо совершил первый ротер
         self.rotor_shift_right_2 = 0  # сколько сдвигов вправо совершил первый ротер
+
+    def update_rotor(self, rotor_name, position_to_shift):
+        """Метод совершает сдвиг букв в указанном роторе. если -1,то влево, если 1 то вправо"""
+
+        rotor = getattr(self, rotor_name)  # Получить значение атрибута по имени
+        updated_rotor = rotor[position_to_shift:] + rotor[:position_to_shift]  # Сдвиг ротора
+        setattr(self, rotor_name, updated_rotor)  # Обновить атрибут на новый
+        # print("Обновлённый ротер: ", updated_rotor)
 
     def start_position_for_rotors(self, position_string):
         """ В зависимости от первого параметра, что ввёл пользователь, меняем начальную позицию ротеров.
@@ -40,19 +46,9 @@ class Enigma():
         # print(f"{position_string[1]} = ", position_2)
         # print(f"{position_string[2]} = ", position_3)
 
-    def panel_changes(self, symbol):
-        """ Метод производит замену символов согласно правилу, прописанном в соединительной панели"""
-        for j in range(0, len(self.panel), 2):
-            if symbol == self.panel[j]:
-                symbol = self.panel[j + 1]
-            elif symbol == self.panel[j + 1]:
-                symbol = self.panel[j]
-
-        print("Новый символ после panel = ", symbol)
-        return symbol
-
     def first_rotor_change(self, symbol, position_to_shift):
         """Метод совершает изменение символа по первому ротору со смещением вправо"""
+
         for i in range(len(self.alphabet)):
             if self.alphabet[i] == symbol:
                 # Замена конкретного символа в конкретном месте в кодирующемся сообщении
@@ -72,6 +68,7 @@ class Enigma():
 
     def first_right_rotor_change(self, symbol, position_to_shift):
         """Метод совершает изменение символа по первому ротору со смещением вправо"""
+
         for i in range(len(self.rotor_1_start)):
             if self.rotor_1_start[i] == symbol:
                 # Замена конкретного символа в конкретном месте в кодирующемся сообщении
@@ -90,6 +87,7 @@ class Enigma():
 
     def second_rotor_change(self, symbol, position_to_shift):
         """Метод совершает изменение символа по второму ротору со смещением влево"""
+
         for i in range(len(self.rotor_2)):
             if self.alphabet[i] == symbol:
                 # Замена конкретного символа в конкретном месте в кодирующемся сообщении
@@ -105,6 +103,7 @@ class Enigma():
 
     def second_right_rotor_change(self, symbol, position_to_shift):
         """Метод совершает изменение символа по второму ротору со смещением вправо"""
+
         for i in range(len(self.rotor_2_start)):
             if self.rotor_2_start[i] == symbol:
                 # Замена конкретного символа в конкретном месте в кодирующемся сообщении
@@ -119,6 +118,7 @@ class Enigma():
 
     def third_rotor_change(self, symbol):
         """Метод совершает изменение символа по третьему ротору влево"""
+
         for i in range(len(self.rotor_3)):
             if self.alphabet[i] == symbol:
                 # Замена конкретного символа в конкретном месте в кодирующемся сообщении
@@ -130,6 +130,7 @@ class Enigma():
 
     def third_rotor_right_change(self, symbol):
         """Метод совершает изменение символа по третьему ротору вправо"""
+
         for i in range(len(self.rotor_3_start)):
             if self.rotor_3_start[i] == symbol:
                 # Замена конкретного символа в конкретном месте в кодирующемся сообщении
@@ -137,6 +138,24 @@ class Enigma():
                 break
 
         print("Новый символ после 3 ротора = ", symbol)
+        return symbol
+
+
+class Enigma(Rotors):
+    def __init__(self, alphabet, panel, rotor_1, rotor_2, rotor_3, reflector):
+        self.panel = panel  # list элементов соединительной панели
+        Rotors.__init__(self, alphabet, rotor_1, rotor_2, rotor_3)
+        self.reflector = reflector  # комбинация рефлектора
+
+    def panel_changes(self, symbol):
+        """ Метод производит замену символов согласно правилу, прописанном в соединительной панели"""
+        for j in range(0, len(self.panel), 2):
+            if symbol == self.panel[j]:
+                symbol = self.panel[j + 1]
+            elif symbol == self.panel[j + 1]:
+                symbol = self.panel[j]
+
+        print("Новый символ после panel = ", symbol)
         return symbol
 
     def reflectors_change(self, symbol):
@@ -150,10 +169,3 @@ class Enigma():
                 break
         print("Новый символ после рефлектора = ", symbol)
         return symbol
-
-    def update_rotor(self, rotor_name, position_to_shift):
-        """Метод совершает сдвиг букв в указанном роторе. если -1,то влево, если 1 то вправо"""
-        rotor = getattr(self, rotor_name)  # Получить значение атрибута по имени
-        updated_rotor = rotor[position_to_shift:] + rotor[:position_to_shift]  # Сдвиг ротора
-        setattr(self, rotor_name, updated_rotor)  # Обновить атрибут на новый
-        # print("Обновлённый ротер: ", updated_rotor)
