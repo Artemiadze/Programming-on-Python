a2aef324c736884491266542eb9dc1c756629141
Artemiadze <t0mi.v@yandex.ru>
2024-12-07 11:59:11 +0300

    add test to enigma classes

--- Homework/Машина_Энигма/tests.py
+++ Homework/Машина_Энигма/tests.py
@@ -7,6 +7,8 @@ def enigma_machine():
     """ Функция создаёт и возвращает созданный здесь класс Энигма с заданными в этой же
     функции атрибутами для этого класса"""
 
+    # настройки энигмы такие простые, чтобы было легче самому находить решения,
+    # а потом через тесты сравнивать, совпало ли с машиной
     alphabet = "АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
     panel = "АБВГДЕ"
     rotor_1 = "БВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯА"
@@ -15,15 +17,15 @@ def enigma_machine():
     reflector = "АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
     return Enigma(alphabet, panel, rotor_1, rotor_2, rotor_3, reflector)
 
-# не трогать
+
 def test_update_rotor(enigma_machine):
-    """ Проверка функции сдвига ротора на N позиций"""
+    """ Проверка функции сдвига ротора на N (здесь N = 1)позиций"""
     original_rotor = enigma_machine.rotor_1
     enigma_machine.update_rotor("rotor_1", 1)
     new_rotor = enigma_machine.rotor_1
     assert original_rotor != new_rotor, "Сдвиг ротора некорректен!"
 
-# не трогать
+
 def test_connecting_panel(enigma_machine):
     """ Метод шифрует сообщение только с использованием соединительной панели, затем обратно
     дешифрует только с использованием соединительной панели. Так мы проверям работу соед. панели"""
@@ -36,27 +38,40 @@ def test_connecting_panel(enigma_machine):
 @pytest.mark.parametrize("start_positions,expected_rotors", [
     ("ААА", ("БВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯА",
              "ВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБ",
-             "ГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВ")),
+             "ГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВ")),  # Тест пройдёт
     ("БВГ", ("ВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБ",
              "ГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВ",
-             "ДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГ")),
+             "ДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГ")),  # Тест не должен пройти - должен выдать ошибку(и он её выдаст)
+    ("БББ", ("ВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБ",
+             "ГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВ",
+             "ДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГ")),  # Тест пройдёт
+    ("ВВД", ("ГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВ",
+             "ДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГ",
+             "ЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЖ"))  # Тест пройдёт
 ])
 def test_start_positions(enigma_machine, start_positions, expected_rotors):
-    """Проверка установки начальных позиций роторов."""
-    enigma_machine.start_position_for_rotors(start_positions)
-    assert enigma_machine.rotor_1 == expected_rotors[0]
-    assert enigma_machine.rotor_2 == expected_rotors[1]
-    assert enigma_machine.rotor_3 == expected_rotors[2]
+    """Проверка установки начальных позиций роторов,
+    действительно ли работает в энигме сдвиг роторов"""
 
-# Не трогать
+    enigma_machine.start_position_for_rotors(start_positions)
+    assert enigma_machine.rotor_1 == expected_rotors[0], "Ротор 1 не совершил необходимый сдвиг при запуске машины"
+    assert enigma_machine.rotor_2 == expected_rotors[1], "Ротор 2 не совершил необходимый сдвиг при запуске машины"
+    assert enigma_machine.rotor_3 == expected_rotors[2], "Ротор 3 не совершил необходимый сдвиг при запуске машины"
+
+
 @pytest.mark.parametrize("message,encrypted", [
     ("АБВ", "ЖЖЛ"),  # правильный ответ
-    ("ГДЕ", "ЖИК"),  # неправильый ответ
+    ("ГДЕ", "ЖИК"),  # неправильый ответ - должен выдать ошибку(наглядная демонстрация ошибки ручного расчёта тестов )
+    ("ГАВ", "ККЛ"),   # правильный ответ
+    ("АРТЕМ", "ЖШЩНХ"),   # правильный ответ
+    ("ВЛАСОВ", "ИСИЩЧР"),   # правильный ответ
 ])
 def test_message_encryption(enigma_machine, message, encrypted):
     """Проверка корректности шифрования сообщений."""
+
     result = ""
     for char in message:
+        # полный цикл кодирования всего сообщения по каждой букве
         enigma_machine.start_position_for_rotors("ААА")
         char = enigma_machine.panel_changes(char)
         char = enigma_machine.first_rotor_change(char)
@@ -64,9 +79,9 @@ def test_message_encryption(enigma_machine, message, encrypted):
         char = enigma_machine.third_rotor_change(char)
         char = enigma_machine.reflectors_change(char)
         result += char
-    assert result == encrypted
+    assert result == encrypted, "Машина закодировала сообщение не так как надо тестироващику"
 
-# Не трогать
+
 @pytest.mark.parametrize("message", [
     123,  # Некорректные символы
     "",  # Пустая строка
@@ -74,9 +89,12 @@ def test_message_encryption(enigma_machine, message, encrypted):
 ])
 def test_incorrect_input(enigma_machine, message):
     """Проверка обработки разных вариаций некорректного ввода."""
+
+    # Проверяем различные Exception в Pytest
     with pytest.raises(Exception):
         result = ""
         for char in message:
+            # полный цикл кодирования всего сообщения по каждой букве
             enigma_machine.start_position_for_rotors("ААА")
             char = enigma_machine.panel_changes(char)
             char = enigma_machine.first_rotor_change(char)
@@ -85,55 +103,42 @@ def test_incorrect_input(enigma_machine, message):
             char = enigma_machine.reflectors_change(char)
             result += char
 
-# Не трогать
-def test_different_rotors():
+
+def test_different_rotors(enigma_machine):
     """Проверка, что разные начальные положения роторов дают разные закодированные сообщения"""
-    alphabet = "АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-    panel = "АБВГ"
-    rotor_1 = "БВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯА"
-    rotor_2 = "ВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБ"
-    rotor_3 = "ГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВ"
-    reflector = "АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
 
-    # Для одного сообщения запускаем параллельно две машины с разными стартовыми положениями роторов,
-    # остальные настройки идентичные
-    enigma_1 = Enigma(alphabet, panel, rotor_1, rotor_2, rotor_3, reflector)
-    enigma_2 = Enigma(alphabet, panel, rotor_2, rotor_1, rotor_3, reflector)
-
-    message = "ГАВ"
+    message = "ГАВ"     # Наше сообщение
     rotors_position_1 = 'ААА'   # Первая настройка роторов
     rotors_position_2 = 'ААБ'   # Вторая настройка роторов
-    encrypted_1 = ""
-    encrypted_2 = ""
+    encrypted_1 = ""    # Результат шифрования при rotors_position_1
+    encrypted_2 = ""    # Результат шифрования при rotors_position_2
 
     # Прогон каждой буквы сообщения через Энигму (имитация ввода с клавиатуры Энигмы буквы для шифровки)
     for char_1 in message:
-        enigma_1.start_position_for_rotors(rotors_position_1)
-        char_1 = enigma_1.panel_changes(char_1)
-        char_1 = enigma_1.first_rotor_change(char_1)
-        char_1 = enigma_1.second_rotor_change(char_1)
-        char_1 = enigma_1.third_rotor_change(char_1)
-        char_1 = enigma_1.reflectors_change(char_1)
-        char_1 = enigma_1.third_rotor_right_change(char_1)
-        char_1 = enigma_1.second_right_rotor_change(char_1)
-        char_1 = enigma_1.first_right_rotor_change(char_1)
-        char_1 = enigma_1.panel_changes(char_1)
+        enigma_machine.start_position_for_rotors(rotors_position_1)
+        char_1 = enigma_machine.panel_changes(char_1)
+        char_1 = enigma_machine.first_rotor_change(char_1)
+        char_1 = enigma_machine.second_rotor_change(char_1)
+        char_1 = enigma_machine.third_rotor_change(char_1)
+        char_1 = enigma_machine.reflectors_change(char_1)
+        char_1 = enigma_machine.third_rotor_right_change(char_1)
+        char_1 = enigma_machine.second_right_rotor_change(char_1)
+        char_1 = enigma_machine.first_right_rotor_change(char_1)
+        char_1 = enigma_machine.panel_changes(char_1)
         encrypted_1 += char_1
 
     # Прогон того же сообщения, но с другими начальными настройками ротеров
     for char_2 in message:
-        enigma_2.start_position_for_rotors(rotors_position_2)
-        char_2 = enigma_2.panel_changes(char_2)
-        char_2 = enigma_2.first_rotor_change(char_2)
-        char_2 = enigma_2.second_rotor_change(char_2)
-        char_2 = enigma_2.third_rotor_change(char_2)
-        char_2 = enigma_2.reflectors_change(char_2)
-        char_2 = enigma_2.third_rotor_right_change(char_2)
-        char_2 = enigma_2.second_right_rotor_change(char_2)
-        char_2 = enigma_2.first_right_rotor_change(char_2)
-        char_2 = enigma_2.panel_changes(char_2)
+        enigma_machine.start_position_for_rotors(rotors_position_2)
+        char_2 = enigma_machine.panel_changes(char_2)
+        char_2 = enigma_machine.first_rotor_change(char_2)
+        char_2 = enigma_machine.second_rotor_change(char_2)
+        char_2 = enigma_machine.third_rotor_change(char_2)
+        char_2 = enigma_machine.reflectors_change(char_2)
+        char_2 = enigma_machine.third_rotor_right_change(char_2)
+        char_2 = enigma_machine.second_right_rotor_change(char_2)
+        char_2 = enigma_machine.first_right_rotor_change(char_2)
+        char_2 = enigma_machine.panel_changes(char_2)
         encrypted_2 += char_2
 
     assert encrypted_1 != encrypted_2, "Сообщение не поменялось "
-
-
